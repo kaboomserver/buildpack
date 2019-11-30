e332eba9004db8921bda4923a5dc3f0fe57ac015
mathiascode <mail@mathias.is>
2019-11-30 20:37:28 +0200

    Add Java files to buildpack

--- script/init.sh
+++ script/init.sh
@@ -2,7 +2,7 @@
 
 # This is the core script that Heroku uses when booting up a dyno
 
-PATH="$HOME/detach/bin/:$HOME/java/bin/:$PATH"
+PATH="$HOME/dtach/bin/:$HOME/java/bin/:$PATH"
 
 # Set up SSH for proxy server and schematics repository
 # Load keys from environmental variables
