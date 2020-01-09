f96efa50be427b774903f59912e701ea622fc666
mathiascode <mail@mathias.is>
2020-01-09 22:28:34 +0200

    Add back log check

--- script/alivecheck.sh
+++ script/alivecheck.sh
@@ -14,7 +14,8 @@ while true; do
 		# If localhost:25565 doesn't respond to ping, or if the log file is older than
 		# 3 minutes, kill the server
 
-		if [ "$(env printf '\xFE' | nc -w 15 localhost 25565 | wc -m)" -eq 0 ]; then
+		if [ "$(env printf '\xFE' | nc -w 15 localhost 25565 | wc -m)" -eq 0 ] ||
+			[ "$(( $(date +%s) - $(date -r $logfile +%s) ))" -gt 300 ]; then
 			if [ "$(tail -20 $logfile | grep -c 'ERROR]: Requested chunk')" -eq 1 ]; then
 				rm -rf $HOME/worlds/
 			fi
