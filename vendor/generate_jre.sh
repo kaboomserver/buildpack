eb2531b46d3bfe2d88f187dee7befeafc0735e8e
mathiascode <mail@mathias.is>
2020-04-16 05:30:25 +0300

    Upgrade to Java 14

--- vendor/generate_jre.sh
+++ vendor/generate_jre.sh
@@ -3,7 +3,7 @@
 # This script is used as a reference to generate a stripped-down OpenJ9 JRE for the server
 
 rm -rf java/
-wget https://github.com/AdoptOpenJDK/openjdk13-binaries/releases/download/jdk-13.0.1%2B9_openj9-0.17.0/OpenJDK13U-jdk_x64_linux_openj9_13.0.1_9_openj9-0.17.0.tar.gz
+wget https://github.com/AdoptOpenJDK/openjdk14-binaries/releases/download/jdk-14%2B36.1_openj9-0.19.0/OpenJDK14U-jdk_x64_linux_openj9_14_36_openj9-0.19.0.tar.gz
 tar -zxvf OpenJDK*
 rm OpenJDK*
 mv jdk* jdk/
