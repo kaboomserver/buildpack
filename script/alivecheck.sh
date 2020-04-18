bec0aa9c970b6d1c1f44168f03df77abc550e05f
mathiascode <mail@mathias.is>
2020-04-18 03:46:51 +0300

    Stop pointing to play.kaboom.pw

--- script/alivecheck.sh
+++ script/alivecheck.sh
@@ -11,7 +11,7 @@ while true; do
 	# If server doesn't respond to ping, or if the log file is older than
 	# 3 minutes, kill the server
 
-	if [ "$(env printf '\xFE' | nc -w 15 play.kaboom.pw 25565 | wc -m)" -eq 0 ]; then
+	if [ "$(env printf '\xFE' | nc -w 15 152.67.69.20 25565 | wc -m)" -eq 0 ]; then
 		pkill -9 java
 		kill -9 $(ps -C ssh -o pid=)
 		echo $(date) >> ~/kill.log
